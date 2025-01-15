2ba4cef0bbb1fd73e1042ea7fffb8680c071e340
EsinShadrach <esinshadrachrafe@gmail.com>
2025-01-15 03:20:22 +0100

    Added notes endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { notes } from "./note";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .use(notes)
+  .get("/", "Hello From Hanger!!")
+  .get("/user/:id", ({ params: { id } }) => id, {
+    params: t.Object({ id: t.String() }),
+  })
+  .get("/rafe", "Shadrach Esin")
+  .post("/form", ({ body, error }) => {
+    console.log(body);
+    return body;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
